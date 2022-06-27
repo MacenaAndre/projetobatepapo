2d6d8406151c854df84a0d617c6a27aa5487ac6f
macenaandre <andremacenacosta@gmail.com>
2022-06-27 12:36:26 -0300

    Versão Final: Requisitos básicos + bônus tela de entrada

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let user = prompt("Qual seu nome de usuário?");
+let user = document.querySelector(".enter").value;
 let Idintervalstatus;
 let Idintervalmessage;
 let messagelist;
-let stat;
 
 function eraseValue (element) {
     if(element.value === "Escreva aqui...") {
        element.value = "";
+       element.classList.remove("sample");
     }
+    if(element.value === "Digite o seu nome") {
+       element.value = "";
+    }
+}
+function login() {
+    user = document.querySelector(".enter").value;
+    document.querySelector(".separate").innerHTML =`
+    <img class="load-img" src="https://acegif.com/wp-content/uploads/loading-45.gif" alt="">
+    <p class="load">Entrando...</p>
+    `
+    enterRoom();
 }
 function enterRoom () {
     let userobject = {
-        name: `${user}`
+        name: user
     }
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userobject);
     promise.catch(userError);
@@ -42,22 +53,21 @@ function getMessages() {
     clearInterval(Idintervalmessage);
 }
 function messagesOk(list) {
-      console.log("carregadas");
       messagelist = list.data;
       const interval = setInterval(getMessages, 3000);
       Idintervalmessage = interval;
       printMessages();
 }
 function messagesEroor(error) {
-    alert("Erro carrregar mensagens");
-    enterRoom();
+    alert("Erro ao carregar mensagens.");
+    window.location.reload();
 }
 function userStatus() {
     let userobject = {
         name: `${user}`
     }
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userobject);
-    promise.catch(userOffline)
+    promise.catch(userOffline);
     promise.then(userOnline);
 
     clearInterval(Idintervalstatus);
@@ -65,12 +75,33 @@ function userStatus() {
 function userOnline(response1) {
     let interval = setInterval(userStatus, 5000);
     Idintervalstatus = interval;
-    stat = "online";
     console.log("online");
 }
 function userOffline(error1) {
-    alert("offline");
-    stat = "offline";
+    alert("Você saiu da sala.");
+    window.location.reload();
+}
+function sendMessage() {
+    const messageobject = {
+      from: user,
+	  to: "Todos",
+	  text: document.querySelector(".typing-space").value,
+	  type: "message" 
+    }
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", messageobject);
+
+    promise.catch(sendFail);
+    promise.then(sendOk);
+
+
+}
+function sendOk(response) {
+    document.querySelector(".typing-space").value = "Escreva aqui...";
+    document.querySelector(".typing-space").classList.add("sample");
+}
+function sendFail(error) {
+    alert("Erro ao enviar mensagens");
     window.location.reload();
 }
 function printMessages() {
@@ -79,32 +110,49 @@ function printMessages() {
 
     for(let i = 0; i < messagelist.length; i++) {
       if(messagelist[i].type === "status" && i < 99) {
-         element.innerHTML += `
-         <div class="status"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> ${messagelist[i].text}</div>`;
+        element.innerHTML += `
+        <div class="status">
+          <p><span class="time">(${messagelist[i].time}) </span><span class="user">${messagelist[i].from} </span>${messagelist[i].text}</p>
+        </div>
+        `;
       }
       if(messagelist[i].type === "status" && i === 99) {
         element.innerHTML += `
-        <div class="status last"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> ${messagelist[i].text}</div>`;
+        <div class="status last">
+          <p><span class="time">(${messagelist[i].time}) </span><span class="user">${messagelist[i].from} </span>${messagelist[i].text}</p>
+        </div>
+        `;
       }
       if(messagelist[i].type === "message" && i < 99) {
         element.innerHTML += `
-        <div class="public-msg"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+        <div class="public-msg">
+          <p><span class="time">(${messagelist[i].time}) </span><span class="user">${messagelist[i].from} </span>para <span class="user">${messagelist[i].to}: </span>${messagelist[i].text}</p>
+        </div>
+        `;
       }
       if(messagelist[i].type === "message" && i === 99) {
-       element.innerHTML += `
-       <div class="public-msg last"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
-      }
-      if(messagelist[i].type === "private_message" && i < 99) {
         element.innerHTML += `
-        <div class="private-msg"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> reservadamente para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+        <div class="public-msg last">
+          <p><span class="time">(${messagelist[i].time}) </span><span class="user">${messagelist[i].from} </span>para <span class="user">${messagelist[i].to}: </span>${messagelist[i].text}</p>
+        </div>
+         `;
       }
-      if(messagelist[i].type === "private_message" && i === 99) {
-       element.innerHTML += `
-       <div class="private-msg last"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> reservadamente para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+      if(messagelist[i].type === "private_message" && i < 99 && messagelist[i].to === user) {
+        element.innerHTML += `
+        <div class="private-msg">
+          <p><span class="time">(${messagelist[i].time}) </span><span class="user">${messagelist[i].from} </span>reservadamente para <span class="user">${messagelist[i].to}: </span>${messagelist[i].text}</p>
+        </div>  
+        `;
+      }
+      if(messagelist[i].type === "private_message" && i === 99 && messagelist[i].to === user) {
+        element.innerHTML += `
+        <div class="private-msg last">
+          <p><span class="time">(${messagelist[i].time}) </span><span class="user">${messagelist[i].from} </span>reservadamente para <span class="user">${messagelist[i].to}: </span>${messagelist[i].text}</p>
+        </div>
+        `;
       }
     }
     document.querySelector(".last").scrollIntoView();
-    console.log("load");
 }
-enterRoom();
+
 
