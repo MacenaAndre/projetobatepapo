d0ff0cd97d557941da00dab0edfc28a1fac4c77d
macenaandre <andremacenacosta@gmail.com>
2022-06-27 05:59:02 -0300

    Mensagens carregando na tela e scrollando para o fim

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let user = prompt("Qual seu nome de usuário?");
-let Idinterval;
+let Idintervalstatus;
+let Idintervalmessage;
+let messagelist;
+let stat;
 
 function eraseValue (element) {
     if(element.value === "Escreva aqui...") {
@@ -16,8 +19,9 @@ function enterRoom () {
 
 }   
 function userOk(response) {
-    alert("deu bom");
-    userStatus()
+    console.log("deu bom");
+    getMessages();
+    userStatus();
 }
 function userError(error) {
     const statuscode = error.response.status;
@@ -29,6 +33,25 @@ function userError(error) {
         enterRoom();
     }
 }
+function getMessages() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+    promise.catch(messagesEroor);
+    promise.then(messagesOk);
+
+    clearInterval(Idintervalmessage);
+}
+function messagesOk(list) {
+      console.log("carregadas");
+      messagelist = list.data;
+      const interval = setInterval(getMessages, 3000);
+      Idintervalmessage = interval;
+      printMessages();
+}
+function messagesEroor(error) {
+    alert("Erro carrregar mensagens");
+    enterRoom();
+}
 function userStatus() {
     let userobject = {
         name: `${user}`
@@ -37,18 +60,51 @@ function userStatus() {
     promise.catch(userOffline)
     promise.then(userOnline);
 
-    clearInterval(Idinterval);
+    clearInterval(Idintervalstatus);
 }
 function userOnline(response1) {
     let interval = setInterval(userStatus, 5000);
-    Idinterval = interval;
+    Idintervalstatus = interval;
+    stat = "online";
     console.log("online");
 }
 function userOffline(error1) {
-        alert("offline");
+    alert("offline");
+    stat = "offline";
+    window.location.reload();
 }
-function printMessages(object) {
+function printMessages() {
+    const element = document.querySelector(".content");
+    element.innerHTML = "";
 
+    for(let i = 0; i < messagelist.length; i++) {
+      if(messagelist[i].type === "status" && i < 99) {
+         element.innerHTML += `
+         <div class="status"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> ${messagelist[i].text}</div>`;
+      }
+      if(messagelist[i].type === "status" && i === 99) {
+        element.innerHTML += `
+        <div class="status last"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> ${messagelist[i].text}</div>`;
+      }
+      if(messagelist[i].type === "message" && i < 99) {
+        element.innerHTML += `
+        <div class="public-msg"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+      }
+      if(messagelist[i].type === "message" && i === 99) {
+       element.innerHTML += `
+       <div class="public-msg last"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+      }
+      if(messagelist[i].type === "private_message" && i < 99) {
+        element.innerHTML += `
+        <div class="private-msg"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> reservadamente para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+      }
+      if(messagelist[i].type === "private_message" && i === 99) {
+       element.innerHTML += `
+       <div class="private-msg last"><span class="time">(${messagelist[i].time})</span> <span class="user">${messagelist[i].from}</span> reservadamente para <span class="user">${messagelist[i].to}:</span> ${messagelist[i].text}</div>`;
+      }
+    }
+    document.querySelector(".last").scrollIntoView();
+    console.log("load");
 }
 enterRoom();
 
