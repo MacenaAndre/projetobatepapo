e7cb7f2bb4fd6b52b683d92dc7c4e6d3fbd242e8
macenaandre <andremacenacosta@gmail.com>
2022-06-26 23:20:20 -0300

    Função permanecer conectado implementada

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let user = prompt("Qual seu nome de usuário?");
+let Idinterval;
 
-function eraseValue (elemento) {
-    if(elemento.value === "Escreva aqui...") {
-       elemento.value = "";
+function eraseValue (element) {
+    if(element.value === "Escreva aqui...") {
+       element.value = "";
     }
 }
 function enterRoom () {
@@ -10,22 +11,44 @@ function enterRoom () {
         name: `${user}`
     }
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userobject);
-    promise.then(userOk);
     promise.catch(userError);
+    promise.then(userOk);
+
 }   
-function userOk(answer) {
+function userOk(response) {
     alert("deu bom");
-    console.log(answer);
+    userStatus()
 }
-function userError(answer) {
-    const statuscode = answer.response.status;
-    if(statuscode === 400) {
+function userError(error) {
+    const statuscode = error.response.status;
+    if(statuscode !== 200) {
         user = prompt(`
         Ja existe um usuário cadastrado com esse nome.
         Por favor, digite outro nome.
         `) 
         enterRoom();
     }
+}
+function userStatus() {
+    let userobject = {
+        name: `${user}`
+    }
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userobject);
+    promise.catch(userOffline)
+    promise.then(userOnline);
+
+    clearInterval(Idinterval);
+}
+function userOnline(response1) {
+    let interval = setInterval(userStatus, 5000);
+    Idinterval = interval;
+    console.log("online");
+}
+function userOffline(error1) {
+        alert("offline");
+}
+function printMessages(object) {
+
 }
 enterRoom();
 
